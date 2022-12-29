148099bd4ec1055c973e836df78f9c431e141eb9
Jong <jjleejong@gmail.com>
2022-12-28 21:05:46 -0600

    connection to mongoDB added

--- server/config/mongoose.configs.js
+++ server/config/mongoose.configs.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
-const mongoEndpoint = 'mongodb://localhost/';
-const db = 'cryptowatcher';
+const mongoEndpoint = 'mongodb+srv://jong:Jojo-970920@cluster0.cbqlgjn.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose
-    .connect(mongoEndpoint+db)
+    .connect(mongoEndpoint)
     .then(() => console.log('Connection to Database Established'))
     .catch((err) => console.log('Error in connecting to database', err));
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
