cd1f0d6cc450f7a4cc10f2f816becaac4ab3df47
Jong Lee <76450939+Jojo970@users.noreply.github.com>
2022-12-29 21:01:11 -0600

    Update mongoose.configs.js

--- server/config/mongoose.configs.js
+++ server/config/mongoose.configs.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 
-const mongoEndpoint = 'mongodb+srv://jong:Jojo-970920@cluster0.cbqlgjn.mongodb.net/?retryWrites=true&w=majority';
+const mongoEndpoint = 'mongodb://localhost/';
+const db = 'cryptowatcher';
 
 mongoose
-    .connect(mongoEndpoint)
+    .connect(mongoEndpoint+db)
     .then(() => console.log('Connection to Database Established'))
     .catch((err) => console.log('Error in connecting to database', err));
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-    console.log("Connected successfully");
-});
