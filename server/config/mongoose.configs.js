96921968166b68d3a4542c9c349272f0703135ed
Jong Lee <76450939+Jojo970@users.noreply.github.com>
2022-12-29 22:37:44 -0600

    Update mongoose.configs.js

--- server/config/mongoose.configs.js
+++ server/config/mongoose.configs.js
@@ -4,6 +4,9 @@ const mongoEndpoint = 'mongodb://localhost/';
 const db = 'cryptowatcher';
 
 mongoose
-    .connect(mongoEndpoint+db)
+    .connect(mongoEndpoint+db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log('Connection to Database Established'))
     .catch((err) => console.log('Error in connecting to database', err));
