33326f2dee40ac3355b4e7b2f7cb8deb588dc946
Jong Lee <76450939+Jojo970@users.noreply.github.com>
2022-12-29 22:44:58 -0600

    Update user.routes.js

--- server/route/user.routes.js
+++ server/route/user.routes.js
@@ -1,8 +1,8 @@
 const userController = require('../controller/user.controller');
 
 module.exports = (app) => {
-  app.post('/register', userController.register);
-  app.post('/login', userController.login);
-  app.post('/logout', userController.logout);
+  app.post('/api/register', userController.register);
+  app.post('/api/login', userController.login);
+  app.post('/api/logout', userController.logout);
   app.get('/api/current-user', userController.getLoggedInUser);
 };
